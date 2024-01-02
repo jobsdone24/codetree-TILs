765eafb269961c03c2d309b432d87d3538a17bb2
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-02 22:22:28 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int N;
+int map[102][102] = { 0 };
+int dp[102][102] = { 0 };
+
+void show() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << dp[i][j] << " ";
+		}cout << "\n";
+	}cout << "\n";
+}
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	//초기값 설정 지나간 값중 최소값
+
+	dp[0][0] = map[0][0];
+
+	for (int i = 1; i < N; i++) {
+		dp[i][0] = max(dp[i-1][0], map[i][0]);
+		dp[0][i] = max(dp[0][i-1], map[0][i]);
+	}
+
+	//점화식 적용
+	for (int i = 1; i < N; i++) {
+		for (int j = 1; j < N; j++) {
+			dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), map[i][j]);
+		}
+	}
+
+	cout << dp[N - 1][N - 1];
+}
