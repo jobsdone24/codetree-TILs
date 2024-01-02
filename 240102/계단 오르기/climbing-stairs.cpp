a11c41a575de34252007a4231a89d5e84518b0e3
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-02 15:24:38 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/계단 오르기/climbing-stairs.cpp
+++ 240102/계단 오르기/climbing-stairs.cpp
@@ -6,11 +6,11 @@ int main() {
 	int n;
 	cin >> n;
 	s[2] = 1;
-    s[3] = 1;
-	for (int i = 3; i <= 1000; i++) {
+	s[3] = 1;
+	for (int i = 4; i <= n; i++) {
 		s[i] = s[i - 2] + s[i - 3];
-        if(s[i]==0)continue;
-	    else s[i] %= 10007;
+		if (s[i] == 0)continue;
+		else s[i] %= 10007;
 	}
 
 	cout << s[n];
