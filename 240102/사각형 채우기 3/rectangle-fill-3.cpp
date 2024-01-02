99821c4c4e9da330354ada5e546a5d2f1677384b
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-02 16:13:09 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240102/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240102/사각형 채우기 3/rectangle-fill-3.cpp
@@ -5,18 +5,20 @@ const int INF = 1000000007;
 
 int main() {
 	long long s[1004] = { 0 };
-	long long gop[1004]={0};
+	long long gop[1004] = { 0 };
 	int n;
 	cin >> n;
 	s[1] = 1;
 	s[2] = 2;
-	gop[1]=2;
-	gop[2] =4;
+	gop[1] = 2;
+	gop[2] = 4;
 	for (int i = 3; i <= n; i++) {
 		s[i] = s[i - 1] + s[i - 2];
-		gop[i] = 2*gop[i-1] % INF;
+		s[i] %= INF;
+		gop[i] = 2 * gop[i - 1] % INF;
+		cout <<s[i]<<" "<< gop[i] << "\n";
 	}
 
-	cout << (s[n]%INF *gop[n]) % INF - s[n] + 1;
-	
+	cout << (s[n] % INF * gop[n] % INF) % INF - s[n] + 1;
+
 }
