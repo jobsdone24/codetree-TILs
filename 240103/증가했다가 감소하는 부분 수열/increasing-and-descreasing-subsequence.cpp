bb2f59e6e726e8f1da92c87e1a17e985edcb8175
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-03 22:50:23 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 499ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<math.h>
+#include<climits>
+using namespace std;
+int number[1004];
+int dp[1004];
+int N;
+
+void init() {
+	for (int i = 0; i < N; i++) {
+		dp[i] = INT_MIN;
+	}
+	dp[0] = 1;
+}
+
+int find(int limits) {
+	init();
+
+	//증가 수열 만들기
+	for (int i = 0; i <= limits; i++) {
+		for (int j = 0; j < i; j++) {
+			if (number[i] > number[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		if (dp[i] == INT_MIN) dp[i] = 1;
+	}
+
+	//감소 수열 만들기
+	for (int i = limits + 1; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (number[i] < number[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		if (dp[i] == INT_MIN) dp[i] = 1;
+	}
+
+	int big = 0;
+	for (int i = 0; i < N; i++) {
+		if (big < dp[i]) big = dp[i];
+	}
+	return big;
+}
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> number[i];
+	}
+
+	int ans = 0;
+	for(int i = 0; i < N; i++) {
+		ans = max(ans, find(i));
+	}
+	cout << ans;
+}
