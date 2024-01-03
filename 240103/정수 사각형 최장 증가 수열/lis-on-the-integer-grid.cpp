a86c1cf0a77fe0401b7cd95e2e3e0269e1ae533b
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-03 09:46:29 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1090ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240103/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240103/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,54 +1,50 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
-#include<tuple>
+#include<math.h>
 using namespace std;
 
 int N;
 int dp[503][503] = { 0 };
 int map[503][503] = { 0 };
-void show() {
-	int big = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (big < dp[i][j]) big = dp[i][j];
-		}
+int dy[] = { 0,1,0,-1 }, dx[] = { 1,0,-1,0 };
+
+int find(int Y, int X) {
+	//만약 값이 정해졌다면
+	if (dp[Y][X] == -1) {
+		return dp[Y][X];
 	}
-	cout << big;
+	else {
+		//자기 자신 초기화
+		int best = 1;
+	
+		for (int i = 0; i < 4; i++) {
+			int ny = Y + dy[i];
+			int nx = X + dx[i];
+			if (ny < 0 || nx < 0 || ny >= N || nx >= N)continue;
+			if (map[ny][nx] > map[Y][X]) {
+				best = max(best, find(ny, nx) + 1);
+			}
+		}
+		return dp[Y][X] = best;
+	}
 }
+
+
 
 int main() {
 	int dy[] = { 0,1,0,-1 }, dx[] = { 1,0,-1,0 };
-
 	cin >> N;
-	
-	vector<tuple<int, int, int>> kan;
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> map[i][j];
-			kan.push_back({ map[i][j],i,j });
-			
-			//dp 초기화
-			dp[i][j] = 1;
 		}
 	}
 	
-	//정렬
-	sort(kan.begin(), kan.end());
-
-	//작은 수 부터 주변 4방향 확인후 1씩 올리기
-	for (int i = 0; i < kan.size(); i++) {
-		int y, x;
-		tie(ignore, y, x) = kan[i];
-		for (int j = 0; j < 4; j++) {
-			int ny = y + dy[j];
-			int nx = x + dx[j];
-			if (ny < 0 || nx < 0 || ny >= N || nx >= N)continue;
-			if (map[ny][nx] > map[y][x]) {
-				dp[ny][nx] = max(dp[ny][nx], dp[y][x] + 1);
-			}
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			ans = max(ans, find(i, j));
 		}
 	}
-	show();
+	cout << ans;
 }
