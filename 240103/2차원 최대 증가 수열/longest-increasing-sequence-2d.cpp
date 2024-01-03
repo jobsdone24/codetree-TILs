72e50b06b9ef9384d4b2e0d69c31a4e144288162
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-03 16:05:19 +0900

    Title: 2차원 최대 증가 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240103/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240103/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -1,54 +1,71 @@
-#include<iostream>
-#include<climits>
-#define MAX_M 53
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_NUM 50
 
 using namespace std;
 
-int map[MAX_M][MAX_M];
-int dp[MAX_M][MAX_M];
+// 변수 선언
 int n, m;
+int grid[MAX_NUM][MAX_NUM];
 
-int main(){
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-			dp[i][j] = INT_MIN;
-		}
-	}
+int dp[MAX_NUM][MAX_NUM];
 
-	//초기값 설정
-	dp[0][0] = 1;
-	for (int t = 1; t < n; t++) {
-		for (int q = 1; q < m; q++) {
-			if (map[0][0] < map[t][q]) {
-				dp[t][q] = max(dp[t][q], dp[0][0] + 1);
-			}
-		}
-	}
+void Initialize() {
+    // 최대를 구해야 하므로
+    // 전부 INT_MIN으로 초기화를 합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            dp[i][j] = INT_MIN;
+    
+    // 초기조건으로는
+    // 시작 위치에 대한 처리가 필요합니다.
+    // 시작 위치에서는 정확히 1칸을 밟게 되는 것이므로
+    // dp[0][0] = 1이 됩니다.
+    dp[0][0] = 1;
+}
 
-	//점화식 적용
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j < m; j++){
-			//각각의 경우에 이중 for문 돌리기
-			for (int t = i + 1; t < n; t++) {
-				for (int q = j + 1; q < m; q++) {
-                    if(dp[t][q]==INT_MIN) continue;
-					if (map[i][j] < map[t][q]) {
-						dp[t][q] = max(dp[t][q], dp[i][j] + 1);
-					}
-				}
-			}
-		}
-	}
+int main() {
+    // 입력:
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> grid[i][j];
+    
+    // 초기값을 설정합니다.
+    Initialize();
 
-	//값 출력
-	int big = 0;
+    // 점화식에 따라
+    // 위에서 아래, 왼쪽에서 오른쪽 방향으로 진행하며
+    // 값을 채워줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++) {
+            // 바로 직전 위치를 (k, l)이라 했을 때
+            // 조건을 만족하는 경우에 대해
+            // 값을 갱신해줍니다.
+            for(int k = 0; k < i; k++)
+                for(int l = 0; l < j; l++) {
+                    // (k, l)에 도달하는 것이 불가하다면
+                    // 패스합니다.
+                    if(dp[k][l] == INT_MIN)
+                        continue;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (big < dp[i][j]) big = dp[i][j];
-		}
-	}
-	cout << big;
+                    // 값이 증가하는 경우에만
+                    // 갱신을 진행합니다.
+                    if(grid[k][l] < grid[i][j])
+                        dp[i][j] = max(dp[i][j], dp[k][l] + 1);
+                }
+        }
+
+    // 어디에 도착하던 상관없이
+    // 최대로 밟을 수 있는 것이 중요하므로
+    // 전체 중 최댓값이 답이 됩니다.
+    int ans = INT_MIN;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            ans = max(ans, dp[i][j]);
+    
+    cout << ans;
+    return 0;
 }
