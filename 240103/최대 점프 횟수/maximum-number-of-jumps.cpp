79c1b5e6f94243f2ae3f7e5b28b0f333801e8304
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-03 15:39:43 +0900

    Title: 최대 점프 횟수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240103/최대 점프 횟수/maximum-number-of-jumps.cpp
+++ 240103/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -1,4 +1,5 @@
 #include<iostream>	
+#include<climits>
 #define MAX_NUM 1000
 using namespace std;
 int jump[MAX_NUM + 1];
@@ -8,16 +9,21 @@ int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		int j;
 		cin >> j;
 		jump[i] = j;
 	}
 
+	//초기화
+	for (int i = 0; i < n; i++) {
+		dp[i] = INT_MIN;
+	}
 	dp[0] = 0;
-	dp[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		for (int j = 1; j < i; j++) {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (dp[j] == INT_MIN) continue;
+
 			if (j + jump[j] >= i) {
 				dp[i] = max(dp[j]+1, dp[i]);
 			}
@@ -26,7 +32,7 @@ int main() {
 
 	//결과값 출력
 	int big = 0;
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		if (dp[i] > big) big = dp[i];
 	}
 	cout << big;
