b1c94ae3e959d7f0f15e4d112fe897a54594cc2b
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-04 21:25:59 +0900

    Title: 배낭 채우기 2, Time: 9ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240104/배낭 채우기 2/knapsack-2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<climits>
+#define MAX_N 100
+#define MAX_M 10000
+using namespace std;
+
+int weight[MAX_N + 1];
+int value[MAX_N + 1];
+
+int dp[MAX_N + 1][MAX_M + 1];
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		int w, v;
+		cin >> w >> v;
+		weight[i] = w;
+		value[i] = v;
+	}
+
+	//값 초기화
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= m; j++) {
+			dp[i][j] = INT_MIN;
+		}
+	}
+	dp[0][0] = 0;
+
+	for (int i = 1; i <= n; i++) {
+		int w = weight[i];
+		int v = value[i];
+		for (int j = 0; j <= m; j++) {
+			// case 1) i번째 아이템을 선택 해서 j가 되는 경우
+			if (j >= w) {
+				dp[i][j] = max(max(dp[i-1][j], dp[i - 1][j - w] + v),dp[i][j-w]+v);
+			}
+			// case 2) i번째 아이템을 선택하지 않고 j가 되는 경우
+			else dp[i][j] = dp[i - 1][j];
+		}
+	}
+
+	// 최대값 찾기
+	int ans = 0;   
+    for(int j=0;j<=m;j++){
+		ans = max(ans, dp[n][j]);
+	}
+	cout << ans;
+}
