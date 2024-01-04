b997918067296fb384288ea78f19281c8070610a
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-04 16:00:11 +0900

    Title: 동전 거슬러주기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240104/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int N,M;
+int coin[103];
+int small[103];
+
+int main(){
+    cin >> N >> M;
+
+    for(int i=1;i<=N;i++) cin >> coin[i];
+
+    //최소값 구하기 -> dp 값을 모두 최대로 만들기
+    for(int i=0;i<=M;i++){
+        small[i] = INT_MAX;
+    }    
+    small[0] = 0;
+
+    for(int i=1;i<=M;i++){
+        for(int j=1; j<=N; j++){
+            int c = coin[j];
+            if(i>=c){
+                if(small[i-c] == INT_MAX) continue;
+                small[i] = min(small[i], small[i-c]+1);
+            }
+        }
+    }
+
+    cout << small[M];
+}
