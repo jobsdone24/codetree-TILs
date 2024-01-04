5ce479c2e3a6a336bc8fac78fe02955d500e97bc
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-04 20:20:25 +0900

    Title: 부분 수열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240104/부분 수열의 합/the-sum-of-the-subsequences.cpp
+++ 240104/부분 수열의 합/the-sum-of-the-subsequences.cpp
@@ -1,33 +1,34 @@
 #include<iostream>
 #include<climits>
+
+#define MAX_N 100
+#define MAX_M 10000
 using namespace std;
 
 int n, m;
 int elements[103];
-int dp[10003];
+
+bool dp[MAX_N+1][MAX_M+1]; //dp[i][j] -> i번째 수까지 고려했을 대 j로 만드는것이 가능하면true 아니면false
 
 int main() {
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
 		cin >> elements[i];
 	}
-
-	//초기화
-	for (int i = 1; i <= m; i++) {
-		dp[i] = INT_MIN;
-	}
 	dp[0] = 0;
 
 	for (int i = 1; i <= n; i++) {
-		for (int j = m; j >= 1; j--) {
-			int e = elements[i];
-			if (j >= e) {
-				if (dp[j - e] == INT_MIN) continue;
-				dp[j] = max(dp[j], dp[j - e] + 1);
-			}
+		//case 1 i번을 포함해서 j를 만드는 경우
+
+		//case 2 i번을 포함하지 않고 j를 만드는 경우
+
+		for(int j=0;j<=m;j++){
+			if(j>=elements[i] && dp[i-1][[j-elements[i]]]) dp[i][j] = true;
+
+			if(dp[i-1][j]) dp[i][j] = true;
 		}
 	}
 
-	if (dp[m] == INT_MIN) cout << "No";
-	else cout << "Yes";
+	if(dp[n][m]) cout << "Yes";
+	else cout << "No"
 }
