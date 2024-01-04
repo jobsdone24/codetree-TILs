ce3d215c7cf140e9c5c117df8c50f03415c367de
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-04 20:27:30 +0900

    Title: 부분 수열의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240104/부분 수열의 합/the-sum-of-the-subsequences.cpp
+++ 240104/부분 수열의 합/the-sum-of-the-subsequences.cpp
@@ -1,34 +1,32 @@
 #include<iostream>
-#include<climits>
-
 #define MAX_N 100
 #define MAX_M 10000
 using namespace std;
 
-int n, m;
-int elements[103];
-
-bool dp[MAX_N+1][MAX_M+1]; //dp[i][j] -> i번째 수까지 고려했을 대 j로 만드는것이 가능하면true 아니면false
+int arr[MAX_N + 1];
+bool dp[MAX_N + 1][MAX_M + 1]; // dp[i][j] -> i번째 원소까지 생각했을 때 j를 만들 수 있으면 true
 
 int main() {
+	int n, m;
 	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+	
 	for (int i = 1; i <= n; i++) {
-		cin >> elements[i];
-	}
-	dp[0] = 0;
+		//i원소를 사용하고 j를 만드는 경우
 
-	for (int i = 1; i <= n; i++) {
-		//case 1 i번을 포함해서 j를 만드는 경우
-
-		//case 2 i번을 포함하지 않고 j를 만드는 경우
-
-		for(int j=0;j<=m;j++){
-			if(j>=elements[i] && dp[i-1][[j-elements[i]]]) dp[i][j] = true;
-
-			if(dp[i-1][j]) dp[i][j] = true;
+		//i원소를 사용하지 않고 j를 만드는 경우
+		for (int j = 0; j <= m; j++) {
+			if (j >= arr[i] && dp[i - 1][j - arr[i]]) {
+				dp[i][j] = true;
+			}
+				
+			if (dp[i - 1][j]) {
+				dp[i][j] = true;
+			}
 		}
 	}
+	if (dp[n][m])cout << "Yes";
+	else cout << "No";
 
-	if(dp[n][m]) cout << "Yes";
-	else cout << "No"
+	return 0;
 }
