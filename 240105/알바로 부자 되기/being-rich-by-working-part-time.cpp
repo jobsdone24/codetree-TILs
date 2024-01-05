46c598baff5d02df797b89031caea913c1546af7
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-05 22:05:46 +0900

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<climits>
+
+#define MAX_N 1000
+
+using namespace std;
+int dp[MAX_N + 1]; // i번째 까지 알바를 진행했을 때 벌 수 있는 최대 돈
+int N;
+int price;
+int arr[MAX_N][3];
+
+
+int main() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+
+		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+		
+		//초기값 등록
+		dp[i] = arr[i][2]; //i번째 일만 했을 때 벌수 있는 돈
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i][0] > arr[i][1]) {
+				dp[i] = max(dp[i], dp[j] + arr[i][2]);
+			}
+		}
+	}
+
+	int ans = 0;
+	for (int i = 1; i <= N; i++) {
+		ans = max(ans, dp[i]);
+	}
+	cout << ans;
+}
