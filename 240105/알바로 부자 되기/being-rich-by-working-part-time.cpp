921ba944155d0524e6076a3daba87fa5898a0308
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-05 22:26:57 +0900

    Title: 알바로 부자 되기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 240105/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -1,38 +1,44 @@
 #include<iostream>
 #include<climits>
-
 #define MAX_N 1000
-
 using namespace std;
-int dp[MAX_N + 1]; // 마지막으로 고른 알바가 i번째 일때 벌 수 있는 최대 돈
-int N;
-int price;
-int arr[MAX_N+1][3];
 
+typedef struct {
+	int s, e, p;
+}S;
+
+int dp[MAX_N + 1]; // dp[i] -> i번째를 마지막으로 진행 했을 때 수익의 최대값
+int N;
+S info[MAX_N + 1];
+
+void Initialize() {
+	for (int i = 0; i <= N; i++) {
+		dp[i] = INT_MIN;
+	}
+	dp[0] = 0;
+}
 
 int main() {
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
-		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+		int s, e, p;
+		cin >> s >> e >> p;
+		info[i] = { s,e,p };
 	}
 
-	//초기값 등록
-	for(int i=0;i<=N;i++){
-		dp[i] = INT_MIN;
-	}
-
-	dp[0] = 0;
+	//초기화
+	Initialize();
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 0; j < i; j++) {
-			if (arr[i][0] > arr[j][1]) {
-				dp[i] = max(dp[i], dp[j] + arr[i][2]);
+			if (info[j].e < info[i].s) {
+				dp[i] = max(dp[i], dp[j] + info[i].p);
 			}
 		}
 	}
 
 	int ans = 0;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i <= N; i++) {
 		ans = max(ans, dp[i]);
 	}
 	cout << ans;
