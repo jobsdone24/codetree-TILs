a1874f1ce92c3fab4b1e6bdf63fb6a1d0992c10b
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-05 22:15:33 +0900

    Title: 알바로 부자 되기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240105/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 240105/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -4,7 +4,7 @@
 #define MAX_N 1000
 
 using namespace std;
-int dp[MAX_N + 1]; // i번째 알바를 진행했을 때 벌 수 있는 최대 돈
+int dp[MAX_N + 1]; // 마지막으로 고른 알바가 i번째 일때 벌 수 있는 최대 돈
 int N;
 int price;
 int arr[MAX_N][3];
@@ -12,16 +12,18 @@ int arr[MAX_N][3];
 
 int main() {
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-
+	for (int i = 1; i <= N; i++) {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-		
-		//초기값 등록
-		dp[i] = arr[i][2]; //i번째 일만 했을 때 벌수 있는 돈
 	}
 
-	dp[0] = arr[0][2];
-	for (int i = 1; i < N; i++) {
+	//초기값 등록
+	for(int i=0;i<=n;i++){
+		dp[i] = INT_MIN;
+	}
+
+	dp[0] = 0;
+
+	for (int i = 1; i <= N; i++) {
 		for (int j = 0; j < i; j++) {
 			if (arr[i][0] > arr[j][1]) {
 				dp[i] = max(dp[i], dp[j] + arr[i][2]);
@@ -30,7 +32,7 @@ int main() {
 	}
 
 	int ans = 0;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		ans = max(ans, dp[i]);
 	}
 	cout << ans;
