f72c50cb5c36394dfdc0b9e3978ca24658a8a4a0
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-06 12:52:54 +0900

    Title: M개의 구간을 선택하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/M개의 구간을 선택하기/select-m-sections.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<climits>
+#define MAX_N 500
+using namespace std;
+
+
+int elements[MAX_N + 1];
+int dp[MAX_N + 1][(MAX_N+1)/2]; //dp[i][j] 마지막으로 i를 선택했을 때, 구간이 j일 때 최대값
+int m;
+
+int main() {
+	int n;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> elements[i];
+
+	//초기값 처리
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			dp[i][j] = INT_MIN;
+		}
+	}
+	dp[1][1] = elements[1];
+	
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			dp[i][j] = max(dp[i - 1][j] + elements[i], dp[i - 2][j - 1] + elements[i]);
+		}
+	}
+
+	int ans = INT_MIN;
+	for (int i = 1; i <= n; i++) {
+		ans = max(ans, dp[i][m]);
+	}
+	cout << ans;
+}
