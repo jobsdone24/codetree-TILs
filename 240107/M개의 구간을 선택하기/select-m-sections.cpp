bc9e6467d0d56d4225bfbb76f27fd64e85dd3030
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-07 13:28:17 +0900

    Title: M개의 구간을 선택하기, Time: 6ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240107/M개의 구간을 선택하기/select-m-sections.cpp
+++ 240107/M개의 구간을 선택하기/select-m-sections.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int elements[MAX_N + 1];
-int dp[MAX_N + 1][(MAX_N + 1) / 2]; //dp[i][j] 마지막으로 i를 선택했을 때, 구간이 j일 때 최대값
+int dp[MAX_N + 1][(MAX_N + 1) / 2][2]; //dp[i][j][p] 마지막으로 i까지 고려했을 때, 선택여부 p이고, 구간이 j일 때 최대값
 int m;
 
 int main() {
@@ -17,24 +17,29 @@ int main() {
 	//초기값 처리
 	for (int i = 0; i <= n; i++) {
 		for (int j = 0; j <= m; j++) {
-			dp[i][j] = MIN_E;
+			for (int p = 0; p < 2; p++) {
+				dp[i][j][p] = MIN_E;
+			}
 		}
 	}
-	dp[0][0] = 0;
-	dp[1][1] = elements[1];
+	dp[0][0][0] = 0;
+	dp[0][0][1] = 0;
+	dp[1][1][1] = elements[1];
+	dp[1][0][0] = 0;
 
 	for (int i = 2; i <= n; i++) {
-		for (int j = 1; j <= (i + 1) / 2; j++) {
+		for (int j = 1; j <= m; j++) {
 			//해당 원소를 선택하는 경우
-			dp[i][j] = max(dp[i-1][j] + elements[i], dp[i - 2][j - 1] + elements[i]);
+			//쭉 수열이 이어질 경우, 새로 시작할 경우
+			int big = max(dp[i - 2][j - 1][0], dp[i - 2][j - 1][1]);
+			dp[i][j][1] = max(dp[i - 1][j][1] + elements[i], big + elements[i]);
 
+			//선택하지 않는 경우
+			dp[i][j][0] = max(dp[i-1][j][1], dp[i - 1][j][0]);		
 		}
 	}
-
-
-	int ans = INT_MIN;
-	for (int i = 1; i <= n; i++) {
-		ans = max(ans, dp[i][m]);
-	}
+	
+	int ans = max(dp[n][m][0], dp[n][m][1]);
 	cout << ans;
+
 }
