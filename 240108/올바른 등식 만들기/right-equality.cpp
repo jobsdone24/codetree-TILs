ab571de62395b27a5d20fb534ab37a585edc14f7
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-08 17:04:30 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240108/올바른 등식 만들기/right-equality.cpp
+++ 240108/올바른 등식 만들기/right-equality.cpp
@@ -5,26 +5,20 @@ using namespace std;
 
 const int MAX_N = 101;
 const int MAX_M = 41;
-const int INT_MIN_VAL = -1e9;  // Integer.MIN_VALUE와 유사
 
 int n, m;
 int arr[MAX_N];
 long long dp[MAX_N][MAX_M];
 
-void init() {
-    for (int i = 0; i <= n; ++i) {
-        for (int j = 0; j <= MAX_M; ++j) {
-            dp[i][j] = 0;
-        }
+int main() {
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> arr[i];
     }
 
-    dp[1][arr[1] + 20]++;
-    dp[1][-1 * arr[1] + 20]++;
-}
-
-void findNumCnt() {
-    init();
-
+    dp[1][arr[1] + 20]=1;
+    dp[1][-1 * arr[1] + 20]=1;
     for (int i = 2; i <= n; ++i) {
         for (int j = 0; j < MAX_M; ++j) {
             if (dp[i - 1][j] == 0) {
@@ -39,21 +33,8 @@ void findNumCnt() {
             }
         }
     }
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> n >> m;
-
-    for (int i = 1; i <= n; ++i) {
-        cin >> arr[i];
-    }
-
-    findNumCnt();
-
-    cout << dp[n][m + 20] << '\n';
+    cout << dp[n][m + 20];
 
     return 0;
 }
