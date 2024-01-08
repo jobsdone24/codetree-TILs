d4046ef8e121ab15095032fc189ac872014eaca3
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-08 17:09:02 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/올바른 등식 만들기/right-equality.cpp
+++ 240108/올바른 등식 만들기/right-equality.cpp
@@ -5,7 +5,7 @@
 #define OFFSET 20
 using namespace std;
 
-long long dp[MAX_N + 1][MAX_M + 1 + OFFSET];
+long long dp[MAX_N + 1][MAX_M + 1 + OFFSET]; 
 // dp[i][j] --> i번째 까지 고려했을 때 합이 j가 되는 수의 최대 수
 int number[MAX_N + 1];
 
@@ -16,33 +16,33 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		cin >> number[i];
 	}
-
+	
 	//초기화
 	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= 40; j++) {
-			dp[i][j] = 0;
+		for (int j = -MAX_M; j <= MAX_M; j++) {
+			dp[i][j + OFFSET] = 0;
 		}
 	}
+	dp[0][0 + OFFSET] = 0;
 	dp[1][number[1] + OFFSET]++;
-	dp[1][-1*number[1] + OFFSET]++;
-
+	dp[1][-number[1] + OFFSET]++;
 	//점화식 적용
 	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j <= 40; j++) {
+		for (int j = -MAX_M; j <= MAX_M; j++) {
 			//만약 범위가 벗어난다면 고려하지 않는다.
-			if (dp[i - 1][j] == 0) continue;
+			if (dp[i - 1][j+ OFFSET] == 0) continue;
 
 			int num = number[i];
 			// case1) 해당 수를 뻬는 경우
-			if ((j - num) >= 0 && (j - num) <= 40) {
-				dp[i][j - num] += dp[i - 1][j];
+			if (j - num >= -MAX_M && j - num <= MAX_M) {
+				dp[i][j - num + OFFSET] += dp[i - 1][j+OFFSET];
 			}
 
 			// case2) 해당 수를 더하는 경우
-			if ((j + num) >= 0 && (j + num) <= 40) {
-				dp[i][j + num] += dp[i - 1][j];
+			if (j + num >= -MAX_M && j + num <= MAX_M) {
+				dp[i][j + num + OFFSET] += dp[i - 1][j+OFFSET];
 			}
 		}
 	}
-	cout << dp[n][m + OFFSET];
+	cout << dp[n][m+OFFSET];
 }
