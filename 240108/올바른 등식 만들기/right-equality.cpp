bbe26bd8a1e5ee1a055767f3d0c6e0b29ca2785c
최형우 <105256600+jobsdone24@users.noreply.github.com>
2024-01-08 16:58:15 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240108/올바른 등식 만들기/right-equality.cpp
+++ 240108/올바른 등식 만들기/right-equality.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 long long dp[MAX_N + 1][MAX_M + 1 + OFFSET]; 
 // dp[i][j] --> i번째 까지 고려했을 때 합이 j가 되는 수의 최대 수
-int number[MAX_N + 1];
+int number[MAX_N + 2];
 
 int main() {
 	int n, m;
@@ -19,7 +19,7 @@ int main() {
 	
 	//초기화
 	for (int i = 0; i <= n; i++) {
-		for (int j = 0;j<=40; j++) {
+		for (int j = 0;j<=41; j++) {
 			dp[i][j] = 0;
 		}
 	}
@@ -28,18 +28,18 @@ int main() {
 	
     //점화식 적용
 	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j <= 40; j++) {
+		for (int j = 0; j <= 41; j++) {
 			//만약 범위가 벗어난다면 고려하지 않는다.
 			if (dp[i - 1][j] == 0) continue;
 
 			int num = number[i];
 			// case1) 해당 수를 뻬는 경우
-			if ((j - num )>= 0 && (j - num) <= 40) {
+			if ((j - num )>= 0 && (j - num) <= 41) {
 				dp[i][j - num] += dp[i - 1][j];
 			}
 
 			// case2) 해당 수를 더하는 경우
-			if ((j + num) >=0 && (j + num )<= 40) {
+			if ((j + num) >=0 && (j + num )<= 41) {
 				dp[i][j + num] += dp[i - 1][j];
 			}
 		}
