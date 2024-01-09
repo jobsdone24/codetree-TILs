2cfd07ae88e4a6bc910e4bb4229bf7f3e20d2f92
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-09 10:44:50 +0900

    Title: 두 수의 합, Time: 113ms, Memory: 6MB, Status: Passed - CodeTree

--- 240109/두 수의 합/sum-of-two-num.cpp
+++ 240109/두 수의 합/sum-of-two-num.cpp
@@ -7,12 +7,13 @@ int main() {
     int n,k;
     vector<int> v;
     unordered_map<int,int> check;
+    unordered_map<int,int> count;
     cin >> n >> k;
     for(int i=0;i<n;i++){
         int num;
         cin >> num;
         v.push_back(num);
-
+        count[num]++;
         check[num] = k-num;
     }
 
@@ -21,7 +22,10 @@ int main() {
         int num = v[i];
 
         if(check.find(num) != check.end()){
-            if(check.find(k-num) != check.end()) cnt++;
+            if(check.find(k-num) != check.end()){
+                if(num == k-num) cnt += count[k-num]-1;
+                else cnt += count[k-num];
+            }
         }
     }
     cout << (cnt+1)/2;
