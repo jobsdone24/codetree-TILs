a8e4449baa375da321c1676b2ef4e7efa699d6d2
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-09 17:17:04 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 354ms, Memory: 2MB, Status: Passed - CodeTree

--- 240109/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
+++ 240109/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -1,25 +1,44 @@
-#include<iostream>
-#include<unordered_map>
-#include<string>
-#include<algorithm> 
+#include <iostream>
+#include <unordered_map>
+#include <string>
 #define MAX_N 1000
-
 using namespace std;
+
+int wordsize[MAX_N];
 
 int main() {
     int n;
     cin >> n;
-    unordered_map<string, int> word;
+    unordered_map<char, int> word[MAX_N];
     for (int i = 0; i < n; i++) {
         string w;
         cin >> w;
-        sort(w.begin(),w.end());
-        word[w]++;
+        
+        wordsize[i] = w.size();
+
+        //단어 개수 확인
+        for (int j = 0; j < w.size(); j++) word[i][w[j]]++;
     }
 
     int ans = 0;
-    for(auto it = word.begin() ; it != word.end(); it++){
-        ans= max(ans,it->second);
+    // N * N 으로 돌면서 확인하기
+    for (int i = 0; i < n; i++) {
+        int group = 0;
+        for (int j = i; j < n; j++) {
+            if (wordsize[i] == wordsize[j]) {
+                bool flag = false;
+                for (auto it = word[i].begin(); it != word[i].end(); it++) {
+                    char temp = it->first;
+                    int cnt = it->second;
+                    if (word[j][temp] != cnt) {
+                        flag = true;
+                        break;
+                    }
+                }
+            if (flag==false) group++;
+            }
+        }
+        ans = max(ans, group);
     }
     cout << ans;
 
