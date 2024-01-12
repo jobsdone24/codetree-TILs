6fbf398fec28693e92bb0513224c96384df12492
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-12 15:37:54 +0900

    Title: 최대 숫자 구하기, Time: 117ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240112/최대 숫자 구하기/find-maximum-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    set<int > s;
+    for(int i=1;i<=m;i++){
+        s.insert(i);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        s.erase(num);
+        cout << *(s.rbegin())<<'\n';
+
+    }
+
+    return 0;
+}
