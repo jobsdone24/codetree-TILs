133931a70b2e3333b8001742048919d80e795aa9
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-14 20:32:37 +0900

    Title: 앞에서부터 삭제하기 2, Time: 46ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240114/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<tuple>
+#include<climits>
+#define MAX_N 10000
+using namespace std;
+
+int numcnt[MAX_N + 1];
+
+int main() {
+	int n;
+	priority_queue<int> pq;
+
+	vector<int> v;
+	cin >> n;
+	int hap = 0;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		hap += num;
+		numcnt[num]++;
+		pq.push(-num);
+		v.push_back(num);
+	}
+
+	double big = 0;
+	
+	for (int i = 0; i < n - 2; i++) {
+		int num = v[i];
+		hap -= num;
+		numcnt[num]--;
+
+		int thap = hap;
+		while (!pq.empty()) {
+			int tmp = -pq.top();
+			pq.pop();
+			if (numcnt[tmp] != 0) {
+				thap -= tmp;
+				double solv = 1.0 * thap / (n - i - 2);
+				if (solv > big) big = solv;
+				break;
+			}
+		}
+	}
+	printf("%.2f", big);
+}
