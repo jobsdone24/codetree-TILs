974a730fd67fd4725ca85723903299ee641984e4
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-14 16:12:03 +0900

    Title: 최대로 연속한 숫자, Time: 451ms, Memory: 10MB, Status: Passed - CodeTree

--- 240114/최대로 연속한 숫자/maximum-consecutive-number.cpp
+++ 240114/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -5,17 +5,24 @@ using namespace std;
 int main() {
 	int n, m;
 	cin >> n >> m;
-	
+
 	set<int> s;
 	set<pair<int, int> > big; // 수열의 최장 길이, 시작값
 	//초기값 등록
 	big.insert({ n + 1,0 });
+	s.insert(-1);
+	s.insert(n+1);
 
 	for (int i = 0; i < m; i++) {
 		int num;
 		cin >> num;
 
-		auto bigit = big.rbegin();
+		auto iter = s.upper_bound(num);
+		int num1 = *iter;
+		iter--;
+		int num2 = *iter;
+		s.insert(num);
+		auto bigit = big.find({ num1 - num2 -1,num2+1 });
 		int st = bigit->second;
 		int ed = st + bigit->first - 1;
 
@@ -24,7 +31,7 @@ int main() {
 		int dist2 = ed - num;
 		big.insert({ dist1,st });
 		big.insert({ dist2, num + 1 });
-		
+
 		cout << big.rbegin()->first << '\n';
 	}
 }
