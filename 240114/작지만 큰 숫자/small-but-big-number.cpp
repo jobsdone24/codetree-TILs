40b074de2667d3d132ee1d23693d72585f2cc365
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-14 16:23:32 +0900

    Title: 작지만 큰 숫자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240114/작지만 큰 숫자/small-but-big-number.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	set<int> s;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		s.insert(num);
+	}
+
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		auto iter = s.upper_bound(num);
+		if (iter == s.end() || iter == s.begin()) {
+			cout << -1 << "\n";
+		}
+		else {
+			iter--;
+			cout << *iter << "\n";
+			s.erase(*iter);
+		}
+	}
+}
