af2053edc92c4a79a26dbe8a2c60f66498b443ee
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-14 22:43:46 +0900

    Title: 배열 추출 2, Time: 61ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240114/배열 추출 2/array-extraction-2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+priority_queue<int> pluspq;
+priority_queue<int> minuspq;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (pluspq.empty() && minuspq.empty()) {
+				cout << 0 << "\n";
+				continue;
+			}
+			if(pluspq.empty()){
+				cout << -minuspq.top() << "\n";
+				minuspq.pop();
+				continue;
+			}
+			if (minuspq.empty()) {
+				cout << -pluspq.top() << "\n";
+				pluspq.pop();
+				continue;
+			}
+			int num = -pluspq.top(); //절대값
+			int num2 = -minuspq.top(); //절대값
+			if (num >= num2) {
+				cout << -num2 << "\n";
+				minuspq.pop();
+			}
+			else {
+				cout << num << "\n";
+				pluspq.pop();
+			}
+		}
+		else if (x > 0) pluspq.push(-x);
+		else minuspq.push(x);
+
+	}
+}
