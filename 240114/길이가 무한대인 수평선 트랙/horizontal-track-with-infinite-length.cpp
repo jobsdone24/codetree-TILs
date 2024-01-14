f26dce14f77ecea83d845d15ce77b5d2d99c6f63
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-14 17:49:43 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240114/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
+++ 240114/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
@@ -1,29 +1,65 @@
 #include<iostream>
 #include<set>
+#include<tuple>
+#define MAX_N 100000
 using namespace std;
 
-int main() {
-	long long n,t;
-	set<long long> s;
+set<pair<int, int> > group;
+int x[MAX_N], v[MAX_N];
 
-	cin >> n >> t;
-	for (int i = 0; i < n; i++) {
-		long long x, v;
-		cin >> x >> v;
 
-		long long target = x + v * t;
-		while (1) {
-			//더이상 없앨 것이 없을 때
-			if (s.lower_bound(target) == s.end()) {
-				break;
-			}
-			else {
-				auto it = s.lower_bound(target);
-				s.erase(it);
-			}
-		}
-		s.insert(target);
+//앞에 있는 사람을 따라잡는 경우 event 등록하기 시간 전에 있는 사람 정보
+set<tuple<double, int, int>> event_t;
+
+void addEvent(int x0, int v0, int x1, int v1) {
+	if (v0 <= v1) return;
+	else {
+		event_t.insert(make_tuple(1.0 * (x1 - x0) / (v0 - v1), x0, v0));
+		return;
 	}
-	cout << s.size();
+}
 
+void eraseEvent(int x0, int v0, int x1, int v1) {
+	if (v0 <= v1) return;
+	else {
+		event_t.erase(make_tuple(1.0 * (x1 - x0) / (v0 - v1), x0, v0));
+		return;
+	}
+}
+
+int main() {
+	int N, T;
+	cin >> N >> T;
+	for (int i = 0; i < N; i++) {
+		cin >> x[i] >> v[i];
+		group.insert({ x[i],v[i] });
+	}
+
+	//전에 사람이 다음 사람을 따라잡는 event 등록
+	for (int i = 0; i < N - 1; i++) {
+		addEvent(x[i], v[i], x[i + 1], v[i + 1]);
+	}
+
+	//event에는 가장 짧은 순서로 저장되어 있음 앞지르는 사건이 있으면 반복
+	while (!event_t.empty()) {
+		double t;
+		int a, b;
+		tie(t, a, b) = *event_t.begin(); //가장 빨리 만나는 두 경우
+		if (t > T) break; // 처음으로 범위를 벗어나면 out 시켜주기
+
+		//만난 경우 각 group과 event 갱신해주기
+		group.erase({ a,b });
+		event_t.erase({ t,a,b });
+
+		//이전 이전에 있는 사람 확인하기
+		auto iter = group.upper_bound({ a,b });
+		if (iter != group.begin()) {
+			iter--;
+			int aa, bb;
+			tie(aa, bb) = *iter;
+			eraseEvent(aa, bb, a, b);
+			addEvent(aa, bb, iter->first, iter->second);
+		}
+	}
+	cout << (int)group.size();
 }
