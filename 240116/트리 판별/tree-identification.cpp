4c2e4869e44111d8df0815eed6563a177ab66b8e
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-16 23:41:08 +0900

    Title: 트리 판별, Time: 1018ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240116/트리 판별/tree-identification.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#define MAX_M 1000
+#define MAX_N 10000
+using namespace std;
+
+vector<int> edge[MAX_N + 1];
+bool visited[MAX_N + 1];
+bool gameover;
+int parent[MAX_N + 1];
+
+void Traversal(int num) {
+	if (gameover)return;
+	for (int i = 0; i < (int)edge[num].size(); i++) {
+		int next = edge[num][i]; // 부모 num, 자식 next
+		if (visited[next] == false) {
+			visited[next] = true;
+			Traversal(next);
+		}
+		else {
+			gameover = true;
+			return;
+		}
+	}
+}
+
+int main() {
+	int m;
+	cin >> m;
+	int last;
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		edge[a].push_back(b);
+		parent[b] = a;
+		last = b;
+	}
+	int root = 0;
+	// 루트 노드 찾기
+	while (1) {
+		int p = parent[last];
+		if (p == 0) {
+			root = last;
+			break;
+		}
+		last = p;
+	}
+
+	//루트노드 방문 처리했을때 visited한곳에 또 간다면 out
+	visited[root] = true;
+	Traversal(root);
+
+	if (gameover == true)cout << 0;
+	else cout << 1;
+}
