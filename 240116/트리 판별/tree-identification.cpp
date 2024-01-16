b909bdfd1253983015b2902e365ef1caa602c4f0
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-16 23:56:12 +0900

    Title: 트리 판별, Time: 1076ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240116/트리 판별/tree-identification.cpp
+++ 240116/트리 판별/tree-identification.cpp
@@ -8,8 +8,10 @@ vector<int> edge[MAX_N + 1];
 bool visited[MAX_N + 1];
 bool gameover;
 int parent[MAX_N + 1];
+int cnt;
 
 void Traversal(int num) {
+	cnt++;
 	if (gameover)return;
 	for (int i = 0; i < (int)edge[num].size(); i++) {
 		int next = edge[num][i]; // 부모 num, 자식 next
@@ -19,24 +21,33 @@ void Traversal(int num) {
 		}
 		else {
 			gameover = true;
-			break;
+			return;
 		}
 	}
-	return;
 }
 
+bool nodecnt[MAX_N + 1];
+int totalcnt;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int m;
 	cin >> m;
-	int last;
+	int last=0;
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
 		edge[a].push_back(b);
 		parent[b] = a;
 		last = b;
+		if (nodecnt[a] == false) {
+			nodecnt[a] = true;
+			totalcnt++;
+		}
+		if (nodecnt[b] == false) {
+			nodecnt[b] = true;
+			totalcnt++;
+		}
 	}
 	int root = 0;
 	// 루트 노드 찾기
@@ -46,13 +57,19 @@ int main() {
 			root = last;
 			break;
 		}
+		if (parent[p] == last) {
+			gameover = true;
+			break;
+		}
 		last = p;
 	}
-
-	//루트노드 방문 처리했을때 visited한곳에 또 간다면 out
-	visited[root] = true;
-	Traversal(root);
-
 	if (gameover == true)cout << 0;
-	else cout << 1;
+	else {
+		//루트노드 방문 처리했을때 visited한곳에 또 간다면 out
+		visited[root] = true;
+		Traversal(root);
+
+		if (gameover == true || cnt != totalcnt )cout << 0;
+		else cout << 1;
+	}
 }
