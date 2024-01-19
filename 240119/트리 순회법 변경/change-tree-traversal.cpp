dc6d649b4bbb4b5923aa645cfb3208008a324415
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 17:09:36 +0900

    Title: 트리 순회법 변경, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240119/트리 순회법 변경/change-tree-traversal.cpp
+++ 240119/트리 순회법 변경/change-tree-traversal.cpp
@@ -1,44 +1,40 @@
-#include<iostream>
-#include<unordered_map>
-#define MAX_N 10'000
+#include <iostream>
+#define MAX_N 10000
+
 using namespace std;
 
-unordered_map<int, int> lnode;
-unordered_map<int, int> rnode;
+// 변수 선언
 int n;
-int a[MAX_N + 1];
+int preorder[MAX_N + 1], postorder[MAX_N + 1];
+int cnt = 1;
 
-void postorder(int x) {
-	if (x == 0)return;
-	postorder(lnode[x]);
-	postorder(rnode[x]);
-	cout << x << "\n";
-
-}
-void check(int start, int end) {
-	if (start >= end) return;
-	bool flag = false;
-	for (int i = start + 1; i <= end; i++) {
-		if (a[start] < a[i]) {
-			//큰 것
-			check(i, end);
-			rnode[a[start]] = a[i];
-			//작은 것
-			check(start + 1, i - 1);
-			if (start + 1 <= i - 1) lnode[a[start]] = a[start + 1];
-			flag = true;
-			return;
+void DFS(int l, int r) {
+	if (l > r) return;
+	if (l == r){
+		postorder[cnt++] = preorder[l];
+		return;
+	}
+	int right_tree = r + 1;
+	for (int i = l + 1; i <= r; i++) {
+		if (preorder[l] < preorder[i]) {
+			right_tree = i;
+			break;
 		}
 	}
-	if (flag == false) {
-		lnode[a[start]] = a[start + 1];
-		check(start + 1, end);
-	}
+
+	DFS(l + 1, right_tree - 1);
+	DFS(right_tree, r);
+	postorder[cnt++] = preorder[l];
 }
 
 int main() {
 	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> a[i];
-	check(1, n);
-	postorder(a[1]);
+	
+	for (int i = 1; i <= n; i++) cin >> preorder[i];
+
+	DFS(1, n);
+	for (int i = 1; i <= n; i++) {
+		cout << postorder[i] << "\n";
+	}
+	return 0;
 }
