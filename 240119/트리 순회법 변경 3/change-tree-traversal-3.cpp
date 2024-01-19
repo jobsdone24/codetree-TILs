b0321770580841a75686af6f97f1b8b06cb99f9f
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 18:06:29 +0900

    Title: 트리 순회법 변경 3, Time: 87ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240119/트리 순회법 변경 3/change-tree-traversal-3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#define MAX_N 100000
+using namespace std;
+
+int preorder[MAX_N + 1], postorder[MAX_N + 1], inorder[MAX_N + 1];
+int cnt = 1;
+int n;
+
+void DFS(int l, int r, int l2, int r2) {
+    if (l > r) return;
+    if (l == r) {
+        preorder[cnt++] = postorder[r];
+        return;
+    }
+
+    int root = 0;
+    for (int i = l2; i <= r2; i++) {
+        if (postorder[r] == inorder[i]) {
+            root = i;
+            break;
+        }
+    }
+
+    int leftsize = root - l2;
+    int rightsize = r2 - root;
+
+    //자기 자신
+    preorder[cnt++] = postorder[r];
+
+    //왼쪽
+    DFS(l, l + leftsize-1, l2, root - 1);
+    //오른쪽   
+    DFS(l + leftsize , r-1, root + 1, r2);
+
+}
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> inorder[i];
+    for (int i = 1; i <= n; i++) cin >> postorder[i];
+
+    DFS(1, n, 1, n);
+
+    for (int i = 1; i <= n; i++) cout << preorder[i] << " ";
+    return 0;
+}
