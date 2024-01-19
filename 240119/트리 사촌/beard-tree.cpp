43517cb1b2b2bca7b3b488d82f4de9be61eb24cd
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 11:23:00 +0900

    Title: 트리 사촌, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240119/트리 사촌/beard-tree.cpp
+++ 240119/트리 사촌/beard-tree.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
-#include<vector>
 #define MAX_N 1000
 using namespace std;
-int node[MAX_N + 1];
-int parent[MAX_N + 1];
 
-int main() {
-	int n, k;
+int a[MAX_N+1];
+int par[MAX_N+1];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n,k;
 	int finding_node;
 	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> node[i];
-		if (k == node[i]) finding_node = i;
+	for(int i=1;i<=n;i++){
+		cin >> a[i];
+		if(a[i] == k) finding_node = i;
 	}
 
-	//트리 만들기
 	int idx = 0;
-	for (int i = 2; i <= n; i++) {
-		if (node[i] > node[i - 1] + 1) idx++;
-		parent[i] = idx;
+
+	for(int i=2;i<=n;i++){
+		if(a[i]> a[i-1]+1) idx++;
+		par[i] = idx;
 	}
 
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (!parent[parent[i]] || !parent[parent[finding_node]]) continue;
-		if (parent[i] != parent[finding_node] && parent[parent[i]] == parent[parent[finding_node]]) ans++;
+	int ans=0;
+	for(int i=1;i<=n;i++){
+		if(!par[par[i]] || !par[par[finding_node]])continue;
+		if(par[i] != par[finding_node] && par[par[i]] == par[par[finding_node]])ans++;
 	}
 	cout << ans;
 }
