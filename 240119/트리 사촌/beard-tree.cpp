d3c5fa17ca82639a37224a3256fbf406dd1b4d36
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 11:10:28 +0900

    Title: 트리 사촌, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240119/트리 사촌/beard-tree.cpp
+++ 240119/트리 사촌/beard-tree.cpp
@@ -26,40 +26,38 @@ void traversal(int x, int depth) {
 int main() {
 	int n, k;
 	int parent[MAX_N + 1] = { 0 };
-	unordered_map<int, int> changer;
+	int finding_node;
 	cin >> n >> k;
 	for (int i = 0; i < n; i++) {
 		cin >> node[i];
-		changer[node[i]] = i;
+		if (k == node[i]) finding_node = i;
 	}
 
 	//트리 만들기
-	int idx = 0; //자식으로 달아햐 할 크기
+	int idx = 0;
 	for (int i = 1; i < n - 1; i++) {
-		int par = idx;
 		if (node[i] + 1 == node[i + 1]) {
 			int si = i;
 			while (node[si] + 1 == node[si + 1]) {
-				edge[par].push_back(si);
-				parent[si] = par;
-                si++;
+				edge[idx].push_back(si);
+				si++;
+				parent[si] = idx;
 			}
-			edge[par].push_back(si);
-			parent[si] = par;
+			edge[idx].push_back(si);
+			parent[si] = idx;
 			i = si;
 			idx++;
 		}
 		else {
-			edge[par].push_back(i);
-			parent[i] = par;
+			edge[idx].push_back(i);
+			parent[i] = idx;
 			idx++;
 		}
 	}
 
 	if (node[n - 2] + 1 != node[n - 1]) {
-		int par = idx;
-		edge[par].push_back(n - 1);
-		parent[n - 1] = par;
+		edge[idx].push_back(n - 1);
+		parent[n - 1] = idx;
 	}
 
 	// 트리 깊이 확하기
@@ -67,10 +65,9 @@ int main() {
 	depthcnt[0] = 1;
 	d[0] = 0;
 	traversal(0, 1);
-
-	k = changer[k];
-	int kpar = parent[k];
+	
+	int kpar = parent[finding_node];
 	int size = edge[kpar].size();
-	int dnum = d[k];
+	int dnum = d[finding_node];
 	cout << depthcnt[dnum] - size;
 }
