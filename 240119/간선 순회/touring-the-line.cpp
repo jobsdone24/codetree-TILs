afeaa1e2089db3d971a0f1a6d979b8c81198a037
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 11:36:14 +0900

    Title: 간선 순회, Time: 93ms, Memory: 4MB, Status: Passed - CodeTree

--- 240119/간선 순회/touring-the-line.cpp
+++ 240119/간선 순회/touring-the-line.cpp
@@ -15,7 +15,7 @@ int dist_node[MAX_N+1];
 pair<int, int> max_dist; // 정점 이동 클수록, 길이 작을 수록
 int last_node;
 
-void DFS(int x) {
+void DFS(int x,int cnt) {
     for (int i = 0; i < edge[x].size(); i++) {
         int y, d;
         tie(y, d) = edge[x][i];
@@ -24,14 +24,13 @@ void DFS(int x) {
 
         visited[y] = true;
         dist[y] = dist[x] + d;
-        dist_node[y] = dist_node[x] + 1;
-        pair<int, int> temp = { dist_node[y],-dist[y] };
+        pair<int, int> temp = {cnt+1,-dist[y] };
         if (max_dist < temp) {
             max_dist = temp;
             last_node = y;
         }
 
-        DFS(y);
+        DFS(y,cnt+1);
     }
 }
 
@@ -49,15 +48,14 @@ int main() {
     }
 
     visited[1] = true;
-    DFS(1);
+    DFS(1,0);
 
     for(int i = 1; i <= n; i++) {
         visited[i] = false;
-        dist_node[i] = 0;
         dist[i] = 0;
     }
     visited[last_node] = true;
-    DFS(last_node);
+    DFS(last_node,0);
     int length = -max_dist.second;
     if (length % d == 0) cout << length / d;
     else cout << (length / d) + 1;
