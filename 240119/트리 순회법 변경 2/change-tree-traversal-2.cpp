006529f93d619864b2d163bd0dfb8f7c4c326e76
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 18:02:47 +0900

    Title: 트리 순회법 변경 2, Time: 117ms, Memory: 1MB, Status: Passed - CodeTree

--- 240119/트리 순회법 변경 2/change-tree-traversal-2.cpp
+++ 240119/트리 순회법 변경 2/change-tree-traversal-2.cpp
@@ -1,45 +1,44 @@
-#include <iostream>
-#define MAX_N 100'000
-
+#include<iostream>
+#define MAX_N 100000
 using namespace std;
 
-// 변수 선언
+int preorder[MAX_N+1], postorder[MAX_N+1],inorder[MAX_N+1];
+int cnt=1;
 int n;
-int preorder[MAX_N + 1],inorder[MAX_N+1], postorder[MAX_N + 1];
-int cnt = 1;
 
-void DFS(int l, int r,int l2,int r2) {
-	if (l > r) return;
-	if (l == r) {
-		postorder[cnt++] = preorder[l];
-		return;
-	}
-	int root = 0;
-	for (int i = l2; i <= r2; i++) {
-		if (preorder[l] == inorder[i])
-			root = i;
-	}
+void DFS(int l,int r, int l2, int r2){
+    if(l>r) return;
+    if(l==r){
+        postorder[cnt++] = preorder[l];
+        return;
+    }
 
-	int leftsize = root - l2;
-	int rightsize = r2 - root;
+    int root = 0;
+    for(int i = l2;i<=r2;i++){
+        if(preorder[l]==inorder[i]){
+            root = i;
+            break;
+        }
+    }
 
+    int leftsize = root-l2;
+    int rightsize = r2 - root;
 
-	DFS(l + 1, l + leftsize, l2, root-1);
-	DFS(l + leftsize + 1, r, root + 1, r2);
-	postorder[cnt++] = preorder[l];
+    //왼쪽
+    DFS(l+1,l+leftsize, l2,root-1);
+    //오른쪽   
+    DFS(l+leftsize+1,r, root+1,r2);
+
+    postorder[cnt++] = preorder[l];
 }
 
-int main() {
-	cin >> n;
-	
-	for (int i = 1; i <= n; i++) cin >> preorder[i];
-	for (int i = 1; i <= n; i++) cin >> inorder[i];
-	
-	DFS(1, n,1,n); //전위 순회 정보, 중위 순회 정보
+int main(){
+    cin >> n;
+    for(int i=1;i<=n;i++) cin >> preorder[i];
+    for(int i=1;i<=n;i++) cin >> inorder[i];
 
-	for (int i = 1; i <= n; i++) {
-		cout << postorder[i] << " ";
-	}
+    DFS(1,n,1,n);
 
-	return 0;
+    for(int i=1;i<=n;i++) cout << postorder[i]<<" ";
+    return 0;
 }
