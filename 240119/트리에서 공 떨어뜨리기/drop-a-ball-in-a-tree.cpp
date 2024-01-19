a3425f243726ed9a0a50d452c06b07c27fe50795
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-19 16:12:55 +0900

    Title: 트리에서 공 떨어뜨리기, Time: 50ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240119/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.cpp
+++ 240119/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int lnode[MAX_N + 1];
 int rnode[MAX_N + 1];
-int ans;
 
-void dfs(int node, long long ball){
-	if(lnode[node]==-1 && rnode[node] ==-1){
-		ans = node;
-		return;
-	}
-	if(lnode[node]==-1) dfs(rnode[node], ball);
-	else if(rnode[node]==-1) dfs(lnode[node],ball);
-	else if(ball%2==0) dfs(rnode[node],ball/2);
-	else dfs(lnode[node],(ball+1)/2);
-
-
-}
 int main() {
 	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> lnode[i] >> rnode[i];
 	}
-	long long k;
+	int k;
 	cin >> k;
-	dfs(1,k);
-	cout << ans;
+	int cur = 1;
+	while (1) {
+		// 리프노드인 경우 (정답)
+		if (lnode[cur] == -1 && rnode[cur] == -1) {
+			cout << cur;
+			break;
+		}
+		else if (lnode[cur] == -1) cur = rnode[cur];
+		else if (rnode[cur] == -1)cur = lnode[cur];
+		else {
+			if (k % 2 == 0)cur = rnode[cur];
+			else cur = lnode[cur];
+			k = (k + 1) / 2;
+		}
+	}
 }
