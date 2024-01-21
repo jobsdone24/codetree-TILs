713595ac78132971f440fa63a3a52a3d8019b583
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-22 01:18:40 +0900

    Title: 노드의 공통 조상 2, Time: 169ms, Memory: 8MB, Status: Passed - CodeTree

--- 240122/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
+++ 240122/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
@@ -23,6 +23,30 @@ void DFS(int x) {
     }
 }
 
+
+int LCA(int a, int b){
+    if(depth[a] < depth[b]) return LCA(b,a);
+
+    //level 1 위치를 같게 해주기
+    if(depth[a] != depth[b]){
+        for(int h=MAX_H;h>=0;h--){
+            if(depth[a] - depth[b] >= (1<<h)) a = parent[h][a];
+        }
+    }
+
+    if(a == b) return a;
+    
+    //level 2 동시에 올리기
+    for(int h=MAX_H;h>=0;h--){
+        if(parent[h][a] != parent[h][b]){
+            a = parent[h][a];
+            b = parent[h][b];
+        }
+    }
+
+    return parent[0][a];
+} 
+
 int main() {
     int n;
     cin >> n;
@@ -32,6 +56,8 @@ int main() {
         edge[a].push_back(b);
         edge[b].push_back(a);
     }
+    visited[1] = true;
+    depth[1] = 1;
     DFS(1);
 
     //점화식 실행
@@ -47,24 +73,6 @@ int main() {
         int a, b;
 
         cin >> a >> b;
-
-        //step 1 높이 같게 만들어주기
-        int big = depth[a] > depth[b] ? a : b;
-        int small = a + b - big;
-
-        if(depth[big] != depth[small]){
-        for (int h = MAX_H; h >= 0; h--) {
-            if (depth[big] - depth[small] >= (1 << h)) big = parent[h][big];
-        }
-        }
-
-        //step 2
-        for (int h = MAX_H; h >= 0; h--) {
-            if (parent[h][big] != parent[h][small]) {
-                big = parent[h][big];
-                small = parent[h][big];
-            }
-        }
-        cout << parent[0][big]<< "\n";
+        cout << LCA(a,b)<<"\n";
     }
 }
