d966fa9f55892449f9dbadda808fdff2b03627dd
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-27 17:52:29 +0900

    Title: 삼 오 무, Time: 100ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240127/삼 오 무/three-five-moo.cpp
+++ 240127/삼 오 무/three-five-moo.cpp
@@ -1,23 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int n, m;
+int n;
 
-bool IsPossible(int target) {
+int IsPossible(int target) {
 	int cnt = 0;
-
-	return cnt >= m;
+	for (int i = 1; i <= target; i++) {
+		if (i % 3 == 0 || i % 5==0) continue;
+		else cnt++;
+		
+		if (cnt > n) return 1;
+	}
+	if (cnt < n)return 2;
 }
 
 int main() {
 	cin >> n;
-	int cnt = 0;
-	for (int i = 1; i <= 1e9; i++) {
-		if (i % 3 == 0 || i % 5 == 0)continue;
-		cnt++;
-        if(cnt==n){
-            cout <<i;
-            break;
-    }
-    }
+	int left = 1;
+	int right = 1e9;
+	int dap = 1e9;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		int num = IsPossible(mid);
+		if (num == 1) right = mid - 1;
+		else{
+            left = mid + 1;
+            dap = min(dap,mid);
+		}
+	}
+	cout << dap;
 }
