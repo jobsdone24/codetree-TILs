69aa3adc81564b8be9f30dced98aaf3a7d7e7cba
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-28 18:48:50 +0900

    Title: 부분문자열이 되는 횟수 구하기, Time: 90ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240128/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#define MAX_N 200'000
+#include<string>
+#include<queue>
+using namespace std;
+
+int arr[MAX_N + 1];
+string A, B;
+int bsize;
+int asize;
+
+bool IsPossible(int idx) {
+	string temp;
+	temp = A;
+	for (int i = 0; i < idx; i++) temp[arr[i]-1] = ' ';
+	int aidx = 0;
+
+	for (int i = 0; i < bsize; i++) {
+		while (B[i] != temp[aidx]) {
+			aidx++;
+			if (aidx == asize)return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+
+	cin >> A >> B;
+	asize = A.size();
+	bsize = B.size();
+
+	for (int i = 0; i < asize; i++) cin >> arr[i];
+	int left = 0;
+	int right = asize - 1;
+	int dap = 0;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		// mid에 해당하는 값까지 지웠을 때 가능한 지 확인하기
+		if (IsPossible(mid)) {
+			left = mid + 1;
+			dap = max(dap, mid);
+		}
+		else right = mid - 1;
+	}
+	if (IsPossible(0))dap++;
+
+	cout << dap;
+}
