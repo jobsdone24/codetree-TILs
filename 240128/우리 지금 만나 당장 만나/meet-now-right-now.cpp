a45aa2b9cb30c37b822f092352d90712dd152658
최형우 <105256600+jobsdone25@users.noreply.github.com>
2024-01-28 17:46:55 +0900

    Title: 우리 지금 만나 당장 만나, Time: 104ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/우리 지금 만나 당장 만나/meet-now-right-now.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#define MAX_N 100'000
+using namespace std;
+
+int n;
+int X[MAX_N + 1];
+int V[MAX_N + 1];
+
+bool IsPossible(double time) {
+	double hap = X[0] + time * V[0];
+	double minus = X[0] - time * V[0];
+	for (int i = 1; i < n; i++) {
+		double xm = X[i] - time * V[i];
+		double xh = X[i] + time * V[i];
+		hap = min(hap, xh);
+		minus = max(minus, xm);
+	}
+	return hap >= minus;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> X[i];
+	for (int i = 0; i < n; i++) cin >> V[i];
+	double lo = 0;
+	double hi = 1e9;
+	double dap = 1e9;
+	
+	for(int i=1;i<=100;i++){
+		double mid = (lo + hi) / 2;
+		if (IsPossible(mid)) {
+			//만약 t초가 가능하다면 더 줄이기
+			hi = mid ;
+			dap = min(dap, mid);
+		}
+		else lo = mid ;
+	}
+	cout << fixed;
+	cout.precision(4);
+	cout << dap;
+}
